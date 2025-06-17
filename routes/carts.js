dcd119f530b56c8acaf1ca334e6124e0591d12d6
schvemler <schvemler.nicolas@gmail.com>
2025-06-17 18:27:01 -0300

    agregando rutas

--- routes/carts.js
+++ routes/carts.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const router = express.Router();
-router.get('/', (req, res) => {
-    res.send('Lista de carritos');
+
+
+let carts = [];
+let nextCartId = 1;
+
+
+router.post('/', (req, res) => {
+    const newCart = {
+        id: nextCartId++, // Autogenerar ID
+        products: []
+    };
+
+    carts.push(newCart);
+    res.status(201).json(newCart);
 });
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    res.send(`Detalles del carrito ${id}`);
+
+router.get('/:cid', (req, res) => {
+    const cart = carts.find(c => c.id === parseInt(req.params.cid));
+    if (cart) {
+        res.json(cart.products);
+    } else {
+        res.status(404).json({ message: 'Carrito no encontrado' });
+    }
 });
 
-module.exports = router;
+
+router.post('/:cid/product/:pid', (req, res) => {
+    const cart = carts.find(c => c.id === parseInt(req.params.cid));
+    if (!cart) {
+        return res.status(404).json({ message: 'Carrito no encontrado' });
+    }
+
+    const { quantity } = req
--- server-express.js
+++ server-express.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-app.get('/', (req, res) => {
-    res.send('¡Hola, mundo!');
-});
+app.use(express.json()); 
+
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
+
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
